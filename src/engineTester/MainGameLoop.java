862ffaefcd44075553145f45cdf80ca26b7ff427
Delta.v <tmm.michalski@gmail.com>
2022-11-14 15:58:10 +0100

    Implement simple polygon rendering

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,17 +2,36 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
 
-        while(!Display.isCloseRequested()) {
-            // game logic
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
 
+        float[] vertices = {
+                -0.5f, 0.5f, 0f, // Left bottom triangle.
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f, // Right top triangle.
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
+        while(!Display.isCloseRequested()) {
+            renderer.prepare();
+            // game logic
+            renderer.render(model);
             //render
             DisplayManager.updateDisplay();
         }
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 }
