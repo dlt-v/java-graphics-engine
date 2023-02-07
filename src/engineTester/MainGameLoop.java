da6576a903455fe85fb46a85978aa2545998d0d3
Delta.v <tmm.michalski@gmail.com>
2023-02-07 11:41:18 +0100

    refactor render classes

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,26 +6,19 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
 
 import java.io.IOException;
 
 public class MainGameLoop {
+
     public static void main(String[] args) throws IOException {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
-
-
-
 
         RawModel model = OBJLoader.loadObjModel("avali", loader);
 
@@ -40,21 +33,19 @@ public class MainGameLoop {
 
         Camera camera = new Camera();
 
+        MasterRenderer renderer = new MasterRenderer();
+
         while(!Display.isCloseRequested()) {
             entity.increaseRotation(0, 1, 0);
             camera.move();
-            renderer.prepare();
-            // game logic
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            shader.stop();
-            //render
+
+            renderer.processEntity(entity);
+            renderer.render(light, camera);
+
             DisplayManager.updateDisplay();
         }
 
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -9,6 +9,9 @@ import shaders.StaticShader;
 import textures.ModelTexture;
 import toolbox.Maths;
 
+import java.util.List;
+import java.util.Map;
+
 public class Renderer {
 
     private static final float FOV = 70;
@@ -16,7 +19,10 @@ public class Renderer {
     private static final float FAR_PLANE = 1000f;
 
     private Matrix4f projectionMatrix;
+    private StaticShader shader;
+
     public Renderer(StaticShader shader) {
+        this.shader = shader;
         GL11.glEnable(GL11.GL_CULL_FACE); // Backface fulling.
         GL11.glCullFace(GL11.GL_BACK);
         createProjectionMatrix();
@@ -32,24 +38,41 @@ public class Renderer {
 
     }
 
-    public void render(Entity entity, StaticShader shader) {
-        TexturedModel model = entity.getModel();
+    public void render(Map<TexturedModel, List<Entity>> entities) {
+        for (TexturedModel model: entities.keySet()) {
+            prepareTexturedModel(model);
+            List<Entity> batch = entities.get(model);
+            for (Entity entity:batch) {
+                prepareInstance(entity);
+                GL11.glDrawElements(GL11.GL_TRIANGLES, model.getRawModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+            }
+            unbindTexturedModel();
+        }
+    }
+
+    private void prepareTexturedModel(TexturedModel model) {
         RawModel rawModel = model.getRawModel();
         GL30.glBindVertexArray(rawModel.getVaoID());
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
         GL20.glEnableVertexAttribArray(2);
-        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(), entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
-        shader.loadTransformationMatrix(transformationMatrix);
+
         ModelTexture texture = model.getTexture();
         shader.loadShineVariables(texture.getShineDamper(), texture.getReflectivity());
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getId());
-        GL11.glDrawElements(GL11.GL_TRIANGLES, rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+    }
+
+    private void unbindTexturedModel() {
         GL20.glDisableVertexAttribArray(0);
         GL20.glDisableVertexAttribArray(1);
         GL20.glDisableVertexAttribArray(2);
         GL30.glBindVertexArray(0);
+    }
+
+    private void prepareInstance(Entity entity) {
+        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(), entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
+        shader.loadTransformationMatrix(transformationMatrix);
     }
 
     private void createProjectionMatrix() {
